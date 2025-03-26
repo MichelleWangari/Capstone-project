43c8645723e7f0ac70633b81eff7c64227b7be37
MichelleWangari <michellewanagri@gmail.com>
2025-03-27 00:13:25 +0300

    added sign up page

---
+++ script.js
@@ -0,0 +1,11 @@
+function onSubmit() {
+    alert ("Form submitted successfully");
+    return true;
+}
+
+function welcomeMsg(name) {
+    name = "Michelle"
+    alert("Hello " + name+ " ! " + " Welcome to our website!")
+}
+
+
